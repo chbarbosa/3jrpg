c86e7d67920ddcafd8831ae8a3821f1ec303d7de
Carlos Henrique Freitas Barbosa <carloshenriquefb@gmail.com>
2026-07-18 18:06:45 -0400

    seed script added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO players (uuid, nickname, email, password_hash, avatar_id, created_at)
+VALUES (
+    '11111111-1111-4111-8111-111111111111',
+    'test',
+    'test@gmail.com',
+    '$2a$10$M3wBkbblt3TYpUZsclmdCOQp3xErPKTryanqvREAF5X9kWWbYATf6',
+    'warrior',
+    CURRENT_TIMESTAMP
+);
