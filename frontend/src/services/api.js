b75a96f9f9ec407fd8398213cc4d02a31dec16f0
Carlos Henrique Freitas Barbosa <carloshenriquefb@gmail.com>
2026-07-31 16:28:14 -0400

    fixes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,7 +7,7 @@ export const setToken = (token) => {
 };
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8080',
 });
 
 api.interceptors.request.use((config) => {
